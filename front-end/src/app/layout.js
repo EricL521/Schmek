e8b4108107009f210386985d4b1fa0a65c0cbcb3
Eric Liu <liueric390@gmail.com>
2023-09-29 17:22:46 -0400

    Changed name of front-end folder

--- front-end/src/app/layout.js
+++ front-end/src/app/layout.js
@@ -9,9 +9,11 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+	return (
+		<html lang="en">
+			<body className={inter.className}>
+				{children}
+			</body>
+		</html>
+	)
 }
