807c7bc633229ff12087db9d52abdbe3fe38cd29
Eric Liu <liueric390@gmail.com>
2023-10-14 15:57:05 -0400

    Theme manager now expands when focused with keyboard

--- src/app/(components)/theme-manager.js
+++ src/app/(components)/theme-manager.js
@@ -6,18 +6,24 @@
 import style from './theme-manager.module.css';
 
 export default function ThemeManager({theme, setTheme, autoHide = true}) {
+	// unFocuses active element if it wasn't selected with tab
+	const unFocus = () => {
+		if (!document.activeElement.matches("*:focus-visible"))
+			document.activeElement.blur();
+	};
+
 	return (
-		<div id={style["theme-manager"]} className={autoHide? null: style["always-visible"]}>
-			<button onClick={() => setTheme('light')} id={style["light"]} 
-			className={[style.interactive, theme == "light" && style.active].join(" ")}>
+		<div id={style["theme-manager"]} className={autoHide? null: style["visible"]}>
+			<button onClick={() => {setTheme('light'); unFocus();}} id={style["light"]} 
+			className={[style['interactive'], theme == "light" && style['selected']].join(" ")}>
 				<img src="./icons/light-mode.svg" alt="Light Theme" draggable="false"/>
 			</button>
-			<button onClick={() => setTheme('system')} id={style["system"]} 
-			className={[style.interactive, theme == "system" && style.active].join(" ")}>
+			<button onClick={() => {setTheme('system'); unFocus();}} id={style["system"]} 
+			className={[style['interactive'], theme == "system" && style['selected']].join(" ")}>
 				<img src="./icons/system-mode.svg" alt="System Theme" draggable="false"/>
 			</button>
-			<button onClick={() => setTheme('dark')} id={style["dark"]}
-			className={[style.interactive, theme == "dark" && style.active].join(" ")}>
+			<button onClick={() => {setTheme('dark'); unFocus();}} id={style["dark"]}
+			className={[style['interactive'], theme == "dark" && style['selected']].join(" ")}>
 				<img src="./icons/dark-mode.svg" alt="Dark Theme" draggable="false"/>
 			</button>
 		</div>
