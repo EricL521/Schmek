885b698393f9e076c8c2a5a12585a8ffadeb11b6
Eric Liu <liueric390@gmail.com>
2023-10-25 22:20:56 -0400

    Fixed bug with death-popup erroring on joining game

--- src/app/(components)/(game-screen)/death-popup.js
+++ src/app/(components)/(game-screen)/death-popup.js
@@ -15,7 +15,7 @@ export default function DeathPopup({ show, stats, respawn }) {
 		<div id={styles['death-popup']} className={[styles['interactive'], !show && styles['hidden']].join(" ")}>
 			<div id={styles['text-div']}>
 				<h1 className={styles['interactive']}>You died :(</h1>
-				<p className={styles['interactive']}>Total Length: {stats.length}</p>
+				<p className={styles['interactive']}>Total Length: {stats?.length}</p>
 			</div>
 			<button className={styles['interactive']} onClick={onButtonClick}>Respawn</button>
 		</div>
