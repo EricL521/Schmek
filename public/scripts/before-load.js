a480b7b4d66b401737391f802e097e7c600a4235
Eric Liu <liueric390@gmail.com>
2023-10-25 22:18:29 -0400

    Fixed before-load occasionally not running in time

--- public/scripts/before-load.js
+++ public/scripts/before-load.js
@@ -1,12 +1,18 @@
 // this runs when the page is loaded
 // runs BEFORE DOM is loaded
 
-// update document background to match theme when window is loaded
-document.addEventListener('DOMContentLoaded', () => {
+const updateBackground = () => {
 	// get theme
 	const theme = localStorage.getItem('theme') ?? 'system';
 	
 	if (theme === 'dark') document.body.style.backgroundColor = "black";
 	else if (theme === 'light') document.body.style.backgroundColor = "white";
 	else document.body.style.backgroundColor = window.matchMedia('(prefers-color-scheme: dark)').matches? 'black': 'white';
-});
+}
+
+// update document background to match theme when window is loaded
+document.addEventListener('DOMContentLoaded', updateBackground);
+
+// if document is already loaded, run code
+if (document.readyState === "complete" || document.readyState === "loaded" || document.readyState === "interactive")
+	updateBackground()
