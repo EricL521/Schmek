2ec92809af70bafa1f63261b0020b13ef4415e8b
Eric Liu <liueric390@gmail.com>
2024-04-09 12:21:16 -0400

    Fake snake now starts out as random direction

--- server-logic/snake-abilities/reverse/fake-snake/fake-snake.js
+++ server-logic/snake-abilities/reverse/fake-snake/fake-snake.js
@@ -1,6 +1,13 @@
-// logic for making the tail that splits off keep moving in the same direction
+// logic for making the tail that splits off and moves in random directions
 
 const reverseAbility = require("../reverse.js");
+
+const directions = new Set([
+	[0, 1],
+	[0, -1],
+	[1, 0],
+	[-1, 0],
+].map(dir => dir.join()));
 
 module.exports = {
 	// no onmount function, because we are keeping the same cooldown
@@ -14,6 +21,20 @@ module.exports = {
 		tailSnake.setDirection(tailSnake.head.directionOut);
 		// revive tail snake
 		game.reviveSnake(tailSnake);
+
+		// on update, change direction to random direction
+		tailSnake.on("updateHeadBorderRadius", (updating) => {
+			if (!updating) return; // only run if we're about to update the head
+
+			const directionsCopy = new Set(directions);
+			// remove opposite current direction from directionsCopy (we can't go backwards)
+			directionsCopy.delete(tailSnake.currentDirection.map(x => -x).join());
+			// randomly choose a direction from directionsCopy
+			const directionString = Array.from(directionsCopy)[Math.floor(Math.random() * directionsCopy.size)];
+			const direction = directionString.split(",").map(num => parseInt(num));
+			
+			tailSnake.setDirection(direction);
+		});
 
 		return output;
 	}
